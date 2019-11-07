c0d3a682341c37da985718ecb7b24c6d7a9cb64a
Laszlo <lcaputo@misena.edu.co>
2019-11-07 18:05:49 -0500

    .

--- script.sh
+++ script.sh
@@ -4,7 +4,7 @@ sudo apt-get update
 sudo apt-get upgrade -y 
 sudo apt-get dist-upgrade -y
 sudo apt-get install sudo
-sudo useradd -u 999 admin
+sudo useradd -m admin
 sudo echo admin:Admin123 | sudo chpasswd
 sudo gpasswd -a  admin sudo
 sudo usermod -aG  sudo admin
