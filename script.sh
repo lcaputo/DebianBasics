5a903cce93236ba1df4d34fea86fb152c707ec8f
Caputo László <L.Caputo27a@gmail.com>
2019-12-27 11:22:47 -0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,4 +1,4 @@
-A#!/bin/bash
+#!/bin/bash
 # RUNTIME - DEBIAN10
 sudo apt-get update
 sudo apt-get upgrade -y 
@@ -13,6 +13,7 @@ sudo apt-get install -y \
     ca-certificates \
     curl \
     gnupg2 \
+    gnome-terminal \
     software-properties-common \
     neofetch \
     lxde \
@@ -32,10 +33,18 @@ sudo add-apt-repository \
    stable"
 sudo apt-get update
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose
-# NODE 
+sudo groupadd docker
+sudo usermod -aG docker tecnico #$(whoami)
+sudo service docker restart
+# MYSQL PORT OPEN
+sudo ufw allow 3306
+# INSTALAR NODE 
 curl -sL https://deb.nodesource.com/setup_12.x | sudo bash -
-#
-sudo apt install -y maven nodejs
+# KEYMAP
+sudo apt install -y console-data maven nodejs
+setxkbmap es sundeadkeys
 sudo npm i @angular/cli -g
 sudo ufw allow 3389 	
 sed -i 's/console/anybody/g' /etc/X11/Xwrapper.config
+# INSTALAR PYTHON
+sudo apt install -y python3 python3-pip
