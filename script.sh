7a2c66b2ca69cc8831ab6f479f5c7f4ca905ffc5
Caputo László <L.Caputo27a@gmail.com>
2020-01-03 13:24:20 -0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -19,37 +19,14 @@ sudo apt-get install -y \
     lxde \
     xrdp \
     ufw 
-# INSTALAR JDK8
-wget -qO - https://adoptopenjdk.jfrog.io/adoptopenjdk/api/gpg/key/public | sudo apt-key add -
-sudo add-apt-repository --yes https://adoptopenjdk.jfrog.io/adoptopenjdk/deb/
-sudo apt update
-sudo apt install -y adoptopenjdk-8-hotspot
-# INSTALAR DOCKER
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
-sudo apt-key fingerprint 0EBFCD88
-sudo add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/debian \
-   $(lsb_release -cs) \
-   stable"
-sudo apt-get update
-sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose
-sudo groupadd docker
-sudo usermod -aG docker tecnico #$(whoami)
-sudo service docker restart
-# MYSQL PORT OPEN
-sudo ufw allow 3306
-# INSTALAR NODE 
-curl -sL https://deb.nodesource.com/setup_12.x | sudo bash -
 # KEYMAP
-sudo apt install -y console-data maven nodejs libreoffice
+sudo apt install -y console-data 
 sudo echo "[Desktop Entry]" > /home/tecnico/.config/autostart/language.desktop
 sudo echo "Type=Application" >> /home/tecnico/.config/autostart/language.desktop
 sudo echo "Exec=setxkbmap latam" >> /home/tecnico/.config/autostart/language.desktop
 sudo echo "[Desktop Entry]" > /home/tecnico/.config/autostart/terminal.desktop
 sudo echo "Type=Application" >> /home/tecnico/.config/autostart/language.desktop
 sudo echo "Exec=gnome-terminal" >> /home/tecnico/.config/autostart/language.desktop
-sudo npm i @angular/cli -g
+# ACTIVAR CONNEXION REMOTA
 sudo ufw allow 3389 	
 sed -i 's/console/anybody/g' /etc/X11/Xwrapper.config
-# INSTALAR PYTHON
-sudo apt install -y python3 python3-pip
