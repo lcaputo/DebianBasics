b527e87a288c4d4832e03f2cdc74f723bf671bcb
Caputo László <L.Caputo27a@gmail.com>
2020-01-03 14:21:05 -0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -41,14 +41,8 @@ sudo service docker restart
 sudo ufw allow 3306
 # INSTALAR NODE 
 curl -sL https://deb.nodesource.com/setup_12.x | sudo bash -
-# KEYMAP
+# EXTRA
 sudo apt install -y console-data maven nodejs libreoffice
-sudo echo "[Desktop Entry]" > /home/tecnico/.config/autostart/language.desktop
-sudo echo "Type=Application" >> /home/tecnico/.config/autostart/language.desktop
-sudo echo "Exec=setxkbmap latam" >> /home/tecnico/.config/autostart/language.desktop
-sudo echo "[Desktop Entry]" > /home/tecnico/.config/autostart/terminal.desktop
-sudo echo "Type=Application" >> /home/tecnico/.config/autostart/terminal.desktop
-sudo echo "Exec=gnome-terminal" >> /home/tecnico/.config/autostart/terminal.desktop
 sudo npm i @angular/cli -g
 sudo ufw allow 3389 	
 sed -i 's/console/anybody/g' /etc/X11/Xwrapper.config
