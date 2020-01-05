03c0951f3d14e2e4157dbe62d39f97437669be28
Caputo László <L.Caputo27a@gmail.com>
2020-01-06 06:23:17 +0700

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 # RUNTIME - DEBIAN10
-sudo apt-get update
-sudo apt-get upgrade -y 
-sudo apt-get dist-upgrade -y
-sudo useradd -m tecnico
-sudo echo tecnico:Admin123 | sudo chpasswd
-sudo gpasswd -a  tecnico sudo
-sudo usermod -aG  sudo tecnico
 sudo apt-get install -y \
     apt-transport-https \
     ca-certificates \
