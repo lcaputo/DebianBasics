653104acb526108750671b04cf5e8dde1a78c3fb
Caputo László <L.Caputo27a@gmail.com>
2020-01-06 06:19:11 +0700

    Update script.sh

--- script.sh
+++ script.sh
@@ -23,34 +23,10 @@ sudo apt --purge autoremove
 sudo apt install -y python3 python3-pip
 # INSTALAR INTERFACE LXDE
 sudo apt-get install -y --no-install-recommends lxde
-# UTILITIES
-sudo apt install -y terminator zsh chromium
-sudo sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
-sudo sed -i 's/robbyrussell/af-magic/g' ~/.zshrc
-chsh -s $(which zsh)
-# INSTALAR JDK8
-wget -qO - https://adoptopenjdk.jfrog.io/adoptopenjdk/api/gpg/key/public | sudo apt-key add -
-sudo add-apt-repository --yes https://adoptopenjdk.jfrog.io/adoptopenjdk/deb/
-sudo apt update
-sudo apt install -y adoptopenjdk-8-hotspot
-# INSTALAR DOCKER
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
-sudo apt-key fingerprint 0EBFCD88
-sudo add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/debian \
-   $(lsb_release -cs) \
-   stable"
-sudo apt-get update
-sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose
-sudo groupadd docker
-sudo usermod -aG docker tecnico
-sudo service docker restart
-# MYSQL PORT OPEN
-sudo ufw allow 3306
-# INSTALAR NODE 
-curl -sL https://deb.nodesource.com/setup_12.x | sudo bash -
 # EXTRA
-sudo apt install -y console-data maven nodejs libreoffice
-sudo npm i @angular/cli -g
+sudo apt install -y console-data 
 sudo ufw allow 3389 	
 sudo sed -i 's/console/anybody/g' /etc/X11/Xwrapper.config
+# UTILITIES
+sudo echo "sh ~/DebianBasics/2.sh" >> ~/.zhsrc
+sudo apt install -y terminator chromium zsh
