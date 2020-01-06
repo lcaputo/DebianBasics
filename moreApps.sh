17eb7e93d3bdca1fb444ee46f54f6282c9714e4c
Caputo László <L.Caputo27a@gmail.com>
2020-01-06 08:27:33 +0700

    Update moreApps.sh

--- moreApps.sh
+++ moreApps.sh
@@ -10,5 +10,4 @@ sudo echo "exit" >> ~/.zhsrc
 sudo sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
 sudo sed -i "s|robbyrussell|af-magic|g" ~/.zshrc
 chsh -s $(which zsh)
-chsh -s $(tecnico zsh)
 sudo sed -i "s|exit| |g" ~/.zshrc
