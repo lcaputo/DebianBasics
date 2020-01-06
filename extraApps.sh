c15effedf02dc80f8c37c3208dc83372a72df99e
Caputo László <L.Caputo27a@gmail.com>
2020-01-06 08:22:17 +0700

    Update and rename test.sh to extraApps.sh

--- extraApps.sh
+++ extraApps.sh
@@ -1,6 +1,14 @@
+######################
+### INSTALL EXTRAS ###
+######################
+# sudo npm i @angular/cli -g
+###################
+### INSTALL ZSH ###
+###################
 sudo apt install -y zsh
 sudo echo "exit" >> ~/.zhsrc
 sudo sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
 sudo sed -i "s|robbyrussell|af-magic|g" ~/.zshrc
 chsh -s $(which zsh)
+chsh -s $(tecnico zsh)
 sudo sed -i "s|exit| |g" ~/.zshrc
